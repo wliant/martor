28c1a2c0c835a9af7cb7ac99820e90d5007ad036
Tan Wei Lian <twl_william89@hotmail.com>
2020-07-17 21:17:10 +0800

    added unit test

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -138,5 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
-MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+#STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
+STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'static')
+#MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'media')
